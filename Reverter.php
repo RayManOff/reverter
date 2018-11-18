85cd69532685e489110dafecea59b790418da8b4
rayman <raymanovg@gmail.com>
2018-11-18 23:51:32 +0300

    [reverter] logic for reverting punctuation marks of text

---
+++ Reverter.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * @author Gadel Raymanov <raymanovg@gmail.com>
+ */
+class Reverter
+{
+    public function revertPunctuationMarks(string $text) : string
+    {
+        $marks = $this->getPunctuationMarks($text);
+        if (empty($marks)) {
+            return $text;
+        }
+
+        $marksPositions = $this->getCharPositions($text, $marks);
+        $reversedMarks = array_combine($marksPositions, array_reverse($marks));
+
+        foreach ($reversedMarks as $position => $mark) {
+            $text[$position] = $mark;
+        }
+
+        return $text;
+    }
+
+    protected function getCharPositions(string $text, array $chars) : array
+    {
+        $positions = [];
+        for ($i = 0; $i < strlen($text); $i++) {
+            if (in_array($text[$i], $chars)) {
+                $positions[] = $i;
+            }
+        }
+
+        return $positions;
+    }
+
+    protected function getPunctuationMarks(string $text) : array
+    {
+        static $pattern = '/[^\w\s\d]/u';
+
+        if (!preg_match_all($pattern, $text, $matches)) {
+            return [];
+        }
+
+        return $matches[0];
+    }
+}
